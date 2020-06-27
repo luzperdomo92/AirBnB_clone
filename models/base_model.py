0cbb9001815347155c6b5425d1c2505dc8d60380
adonis7ejeda <adonis7ejeda@gmail.com>
2020-06-27 02:00:26 +0000

    pep8 base model

--- models/base_model.py
+++ models/base_model.py
@@ -24,7 +24,6 @@ class BaseModel:
                     continue
                 else:
                     setattr(self, key, value)
-                
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
