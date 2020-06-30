5eba4f73a013657952d8f2213c1edc1389f02bb4
Luz Perdomo <lilyperdomo92@gmail.com>
2020-06-30 16:54:33 -0500

    added Review class

---
+++ models/review.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Creating a new class"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """ """
+    place_id = ""
+    user_id = ""
+    text = ""
+
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
