96edef142b9d803005ab79c30df453fb557b61e2
adonis7ejeda <adonis7ejeda@gmail.com>
2020-06-30 14:49:14 +0000

    pep8 test_base_model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -8,59 +8,59 @@ import os
 
 
 class testBaseModelClass(unittest.TestCase):
-      """Test for class BaseModel"""
-      def resetStorage(self):
-            FileStorage._FileStorage__objects = {}
-            if os.path.isfile(FileStorage._FileStorage__file_path):
-                  os.remove(FileStorage._FileStorage__file_path)
+    def resetStorage(self):
+        FileStorage._FileStorage__objects = {}
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
 
-      def test_instantiation(self):
-              b = BaseModel()
-              self.assertEqual(str(type(b)), "<class 'models.base_model.BaseModel'>")
-              self.assertIsInstance(b, BaseModel)
-              self.assertTrue(issubclass(type(b), BaseModel))
+    def test_instantiation(self):
+        b = BaseModel()
+        self.assertEqual(str(type(b)),
+                         "<class 'models.base_model.BaseModel'>")
+        self.assertIsInstance(b, BaseModel)
+        self.assertTrue(issubclass(type(b), BaseModel))
 
-      def test_init_no_args(self):
-            self.resetStorage()
-            with self.assertRaises(TypeError) as e:
-                  BaseModel.__init__()
-            msg = "__init__() missing 1 required positional argument: 'self'"
-            self.assertEqual(str(e.exception), msg)
+    def test_init_no_args(self):
+        self.resetStorage()
+        with self.assertRaises(TypeError) as e:
+            BaseModel.__init__()
+        msg = "__init__() missing 1 required positional argument: 'self'"
+        self.assertEqual(str(e.exception), msg)
 
-      def test_init_many_args(self):
-            self.resetStorage()
-            args = [i for i in range(1000)]
-            b = BaseModel(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
-            b = BaseModel(*args)
+    def test_init_many_args(self):
+        self.resetStorage()
+        args = [i for i in range(1000)]
+        b = BaseModel(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+        b = BaseModel(*args)
 
-      def test_datatime_created(self):
-            date_now = datetime.now()
-            b = BaseModel()
-            diff = b.updated_at - b.created_at
-            self.assertTrue(abs(diff.total_seconds()) < 0.1)
-            diff = b.created_at - date_now
-            self.assertTrue(abs(diff.total_seconds()) < 0.1)
+    def test_datatime_created(self):
+        date_now = datetime.now()
+        b = BaseModel()
+        diff = b.updated_at - b.created_at
+        self.assertTrue(abs(diff.total_seconds()) < 0.1)
+        diff = b.created_at - date_now
+        self.assertTrue(abs(diff.total_seconds()) < 0.1)
 
-      def test_id(self):
-            l = [BaseModel().id for i in range(1000)]
-            self.assertEqual(len(set(l)), len(l))
+    def test_id(self):
+        l = [BaseModel().id for i in range(1000)]
+        self.assertEqual(len(set(l)), len(l))
 
-      def test_to_dict(self):
-            b = BaseModel()
-            b.name = "Adonis"
-            b.age = 22
-            d = b.to_dict()
-            self.assertEqual(d["id"], b.id)
-            self.assertEqual(d["__class__"], type(b).__name__)
-            self.assertEqual(d["created_at"], b.created_at.isoformat())
-            self.assertEqual(d["updated_at"], b.updated_at.isoformat())
-            self.assertEqual(d["name"], b.name)
-            self.assertEqual(d["age"], b.age)
+    def test_to_dict(self):
+        b = BaseModel()
+        b.name = "Adonis"
+        b.age = 22
+        d = b.to_dict()
+        self.assertEqual(d["id"], b.id)
+        self.assertEqual(d["__class__"], type(b).__name__)
+        self.assertEqual(d["created_at"], b.created_at.isoformat())
+        self.assertEqual(d["updated_at"], b.updated_at.isoformat())
+        self.assertEqual(d["name"], b.name)
+        self.assertEqual(d["age"], b.age)
 
-      def test_str(self):
-            b = BaseModel()
-            string = "[BaseModel] ({}) {}".format(b.id, b.__dict__)
-            self.assertEqual(string, str(b))
+    def test_str(self):
+        b = BaseModel()
+        string = "[BaseModel] ({}) {}".format(b.id, b.__dict__)
+        self.assertEqual(string, str(b))
 
 if __name__ == "__main__":
         unittest.main()
