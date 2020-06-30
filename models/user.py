cbfae8f4174a1fb994ee23667de6b072dd3f452b
Luz Perdomo <lilyperdomo92@gmail.com>
2020-06-30 16:03:29 -0500

    change constructor

--- models/user.py
+++ models/user.py
@@ -12,8 +12,4 @@ class User(BaseModel):
     
     def __init__(self, *args, **kwargs):
         """ """
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
         super().__init__(*args, **kwargs)
