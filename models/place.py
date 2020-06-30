3305ad36485be592f86f436b0278c581ff96f64c
Luz Perdomo <lilyperdomo92@gmail.com>
2020-06-30 16:54:05 -0500

    added Pace class

---
+++ models/place.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Creating a new class"""
+from models.base_model import BaseModel
+
+
+class Place(BaseModel):
+    """ """
+    city_id = ""
+    user_id ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
+
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+
+    def set_attribute(self, attr_name, value):
+        """ """
+        if attr_name in ("number_rooms", "number_bathrooms","max_guest",
+                            "price_by_night"):
+            value = int(value)
+        elif attr_name in ("latitude", "longitude"):
+            value = float(value)
+
+        super().set_attribute(attr_name, value)
