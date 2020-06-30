c1d3c86ed6930682f28af2c84134232dffc146e5
Luz Perdomo <lilyperdomo92@gmail.com>
2020-06-30 11:48:16 -0500

    added User class

---
+++ models/user.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Creating a new class"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """ """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+    
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
