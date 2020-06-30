9b616d8e59b0f42246db737cf8baefe957340fc1
Luz Perdomo <lilyperdomo92@gmail.com>
2020-06-30 14:19:47 -0500

    change logical syntax for class

--- models/user.py
+++ models/user.py
@@ -5,11 +5,11 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """ """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
     
     def __init__(self, *args, **kwargs):
         """ """
+        self.email = ""
+        self.password = ""
+        self.first_name = ""
+        self.last_name = ""
         super().__init__(*args, **kwargs)
