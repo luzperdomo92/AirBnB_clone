f55aa6f3958c1de452060b4d4d1f9690ce29c700
Luz Perdomo <lilyperdomo92@gmail.com>
2020-06-30 18:03:24 -0500

    fixed = symbol

--- models/place.py
+++ models/place.py
@@ -6,7 +6,7 @@ from models.base_model import BaseModel
 class Place(BaseModel):
     """ """
     city_id = ""
-    user_id ""
+    user_id = ""
     name = ""
     description = ""
     number_rooms = 0
