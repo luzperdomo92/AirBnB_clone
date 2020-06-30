adfcf37b0451b29a07ebf3abb9afce379947d3ef
Luz Perdomo <lilyperdomo92@gmail.com>
2020-06-30 14:39:07 -0500

    fixed Public class attributes for User

--- models/user.py
+++ models/user.py
@@ -5,6 +5,10 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """ """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
     
     def __init__(self, *args, **kwargs):
         """ """
