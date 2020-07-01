cce92aec3e3d31faaba44f8d571704125d0e050a
adonis7ejeda <adonis7ejeda@gmail.com>
2020-07-01 04:04:29 +0000

    update test models for task 5

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,6 +6,7 @@ import unittest
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 from datetime import datetime
+import json
 import time
 import os
 
@@ -13,13 +14,13 @@ import os
 class testBaseModelClass(unittest.TestCase):
     """a"""
     def resetStorage(self):
-        """c"""
+        """Resets FileStorage data"""
         FileStorage._FileStorage__objects = {}
         if os.path.isfile(FileStorage._FileStorage__file_path):
             os.remove(FileStorage._FileStorage__file_path)
 
     def test_instantiation(self):
-        """f"""
+        """Tests instantiation of BaseModel class"""
         b = BaseModel()
         self.assertEqual(str(type(b)),
                          "<class 'models.base_model.BaseModel'>")
@@ -27,7 +28,7 @@ class testBaseModelClass(unittest.TestCase):
         self.assertTrue(issubclass(type(b), BaseModel))
 
     def test_init_no_args(self):
-        """h"""
+        """Test __init__ with no arguments"""
         self.resetStorage()
         with self.assertRaises(TypeError) as e:
             BaseModel.__init__()
@@ -35,14 +36,14 @@ class testBaseModelClass(unittest.TestCase):
         self.assertEqual(str(e.exception), msg)
 
     def test_init_many_args(self):
-        """i"""
+        """Test __init__ with many arguments"""
         self.resetStorage()
         args = [i for i in range(1000)]
         b = BaseModel(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
         b = BaseModel(*args)
 
     def test_datatime_created(self):
-        """j"""
+        """Tests if updated_at & created_at are current at creation"""
         date_now = datetime.now()
         b = BaseModel()
         diff = b.updated_at - b.created_at
@@ -51,12 +52,12 @@ class testBaseModelClass(unittest.TestCase):
         self.assertTrue(abs(diff.total_seconds()) < 0.1)
 
     def test_id(self):
-        """k"""
+        """Tests for unique user ids"""
         l = [BaseModel().id for i in range(1000)]
         self.assertEqual(len(set(l)), len(l))
 
     def test_3_save(self):
-        """n"""
+        """Tests the public instance method save()"""
         b = BaseModel()
         time.sleep(0.5)
         date_now = datetime.now()
@@ -65,7 +66,7 @@ class testBaseModelClass(unittest.TestCase):
         self.assertTrue(abs(diff.total_seconds()) < 0.01)
 
     def test_to_dict(self):
-        """l"""
+        """Tests the public instance method to_dict()"""
         b = BaseModel()
         b.name = "Adonis"
         b.age = 22
@@ -78,10 +79,40 @@ class testBaseModelClass(unittest.TestCase):
         self.assertEqual(d["age"], b.age)
 
     def test_str(self):
-        """m"""
+        """Tests for __str__ method"""
         b = BaseModel()
         string = "[BaseModel] ({}) {}".format(b.id, b.__dict__)
         self.assertEqual(string, str(b))
+
+    def test_5_save(self):
+        """Tests that storage.save() is called from save()."""
+        self.resetStorage()
+        b = BaseModel()
+        b.save()
+        key = "{}.{}".format(type(b).__name__, b.id)
+        d = {key: b.to_dict()}
+        self.assertTrue(os.path.isfile(FileStorage._FileStorage__file_path))
+        with open(FileStorage._FileStorage__file_path,
+                  "r", encoding="utf-8") as f:
+            self.assertEqual(len(f.read()), len(json.dumps(d)))
+            f.seek(0)
+            self.assertEqual(json.load(f), d)
+
+    def test_5_save_no_args(self):
+        """Tests save() with no arguments"""
+        self.resetStorage()
+        with self.assertRaises(TypeError) as e:
+            BaseModel.save()
+        msg = "save() missing 1 required positional argument: 'self'"
+        self.assertEqual(str(e.exception), msg)
+
+    def test_5_save_excess_args(self):
+        """Tests save() with too many arguments."""
+        self.resetStorage()
+        with self.assertRaises(TypeError) as e:
+            BaseModel.save(self, 98)
+        msg = "save() takes 1 positional argument but 2 were given"
+        self.assertEqual(str(e.exception), msg)
 
 if __name__ == "__main__":
         unittest.main()
